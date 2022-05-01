bf76053ff4f561d15cb9fb0555b2168b50e07cee
Jaisingh2302 <jai.a.singh@Geminisolutions.com>
2022-05-01 18:34:56 +0530

    Table and thead added

---
+++ JavaScript Assignement/Assignement 1/Index.js
@@ -0,0 +1,16 @@
+(function(){
+    let details = [
+    [ "Name", "Age", "DOB", "Email", "Company"],
+    [ "ABC", "18", "12/07/2003", "xyz@gmail.com", "XYZ" ],
+    [ "DEF", "19", "10/10/2001", "abc@gmail.com", "MNO"],
+    [ "GHI", "15", "12/12/2006", "dfe@gmail.com", "SDE"],
+    [ "JKL", "22", "23/02/2004", "jkl@gmail.com", "HSE"],
+    ]
+    var table = document.createElement("table");
+    table.setAttribute("class", "table table-striped");
+    var tHead = document.createElement("tHead");
+    tHead.setAttribute("class", "table-dark");
+
+})();
+   
+   
