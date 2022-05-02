b246ecc403ca2f6a0c5dab0a0c8c254b7e02ca17
Jaisingh2302 <jai.a.singh@Geminisolutions.com>
2022-05-02 14:55:19 +0530

    Apply valid and invalid feedback classes

--- JavaScript Assignement/Assignement 2/two.js
+++ JavaScript Assignement/Assignement 2/two.js
@@ -1,9 +1,8 @@
-var userN , Email , Phone;
-var Nametrue , emailtrue , numbertrue;
+var userN, Email, Phone;
+var Nametrue, emailtrue, numbertrue;
 var count = 1;
 
-function sub(){
-  console.log("one");
+function sub() {
   Nametrue = false;
   emailtrue = false;
   numbertrue = false;
@@ -16,62 +15,69 @@ function sub(){
   validation();
 }
 
-function validation(){
-  console.log("two");
+function validation() {
   var a = /^[A-Za-z]+$/;
   var b = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
   var c = /^\d{10}$/;
 
-  if(a.test(userN) && userN.length!=0){
+  if (a.test(userN) && userN.length != 0) {
     Nametrue = true;
     document.getElementById("name-v").style.display = "none";
-  } else{
+    document.getElementById("name-inv").style.display = "block";
+  } else {
     userN = false;
     document.getElementById("name-v").style.display = "block";
+    document.getElementById("name-inv").style.display = "none";
   }
-  if(b.test(Email) && Email.length!=0){
-    emailtrue= true;
+  if (b.test(Email) && Email.length != 0) {
+    emailtrue = true;
     document.getElementById("email-v").style.display = "none";
-  } else{
+    document.getElementById("email-inv").style.display = "block";
+  } else {
     Email = false;
     document.getElementById("email-v").style.display = "block";
+    document.getElementById("email-inv").style.display = "none";
   }
-  if(c.test(Phone) || Phone.length==0){
+  if (c.test(Phone) || Phone.length == 0) {
     numbertrue = true;
     document.getElementById("num-v").style.display = "none";
-  } else{
-   Phone = false;
+    if(Phone.length==0){
+      document.getElementById("num-inv").style.display = "none";
+    } else{
+      document.getElementById("num-inv").style.display = "block";
+    }
+    
+  } else {
+    Phone = false;
     document.getElementById("num-v").style.display = "block";
+    document.getElementById("num-inv").style.display = "none";
   }
   isvalid();
 }
 
-function isvalid(){
-  console.log("three");
-  if(Nametrue && emailtrue && numbertrue){
+function isvalid() {
+  if (Nametrue && emailtrue && numbertrue) {
     informationCorrect();
   }
 }
 
-function informationCorrect(){
-  console.log("four");
+function informationCorrect() {
   localStorage.setItem(count + "name", userN);
   localStorage.setItem(count + "email", Email);
   localStorage.setItem(count + "number", Phone);
   viewdetails();
 }
 
-function viewdetails(){
-  console.log("five");
+function viewdetails() {
   var table = document.getElementById("tbody");
   let d = document.createElement("tr");
-  // console.log(localStorage);
+
   d.innerHTML = `<tr>
     <th scope = "row">${count}</th>
     <td>${localStorage.getItem(count + "name")}</td>
     <td>${localStorage.getItem(count + "email")}</td>
     <td>${localStorage.getItem(count + "number")}</td>
-  </tr>`
+  </tr>`;
   table.append(d);
   count++;
 }
